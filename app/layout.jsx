d7b80a91763f103f1049986ea7619717e01c1328
matjazo18 <matjaz.gazvoda@gmail.com>
2025-05-22 21:23:58 +0200

    Now people cann see by who it was made

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import { Toaster } from "react-hot-toast";
+import Link from "next/link";
 import { AuthProvider } from "./context/AuthContext";
 const inter = Inter({
   subsets: ["latin"],
@@ -20,6 +21,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
+        <div className="fixed bottom-4 right-4 z-50 max-w-[90px]">
+          <Link
+            href="https://matjazo.com"
+            className="flex items-center gap-1 rounded-xl bg-gray-100 px-4 py-2 text-sm font-light text-gray-700 shadow-md transition hover:bg-gray-200 hover:shadow-lg"
+          >
+            by matjazo
+            <span className="text-[#00ff99] text-xl leading-none">.</span>
+          </Link>
+        </div>
+
         <Toaster position="top-center" />
         <AuthProvider>{children}</AuthProvider>
       </body>
