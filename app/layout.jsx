d7b5b996b02576a57f63150ead18000d208b0f11
matjazo18 <matjaz.gazvoda@gmail.com>
2025-05-23 11:01:32 +0200

    Padding + Link to my page

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,9 @@ import Header from "@/components/Header";
 import { Toaster } from "react-hot-toast";
 import Link from "next/link";
 import { AuthProvider } from "./context/AuthContext";
+import m from "@/public/M.png";
+import Image from "next/image";
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -21,13 +24,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        <div className="fixed bottom-4 right-4 z-50 max-w-[90px]">
+        <div className="fixed bottom-1 right-0 z-50 hidden xl:block max-w-[90px] xl:min-w-[120px]">
           <Link
             href="https://matjazo.com"
-            className="flex items-center gap-1 rounded-xl bg-gray-100 px-4 py-2 text-sm font-light text-gray-700 shadow-md transition hover:bg-gray-200 hover:shadow-lg"
+            className="flex items-center justify-center gap-1 rounded-xl bg-gray-100  py-2 text-sm font-light text-gray-700 shadow-md transition hover:bg-gray-200 hover:shadow-lg"
           >
             by matjazo
-            <span className="text-[#00ff99] text-xl leading-none">.</span>
+            <Image src={m} alt="m" width={20} height={20} />
           </Link>
         </div>
 
